a0432b304bf84301bf1b4604c7e10cba7def113a
Brayan Snehyder Castro Velandia <222234032+01Brxyan@users.noreply.github.com>
2025-08-22 08:32:17 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,9 +5,38 @@ while True:
     print('1. 🤖 Camper 🤖 ')
     print('2. ⌨️  Trainer ⌨️ ')
     print('3. 👽 Coordinador 👽')
+    
 
     opcion= input('Ingresa un numero (1-3): ')
 
     if opcion == "1":
         while True:
-            print(' 🤖 Menu Camper 🤖 ')
+            print()
+            print(' 🤖 Ingreso Camper 🤖 ')
+            ID_camper= input(' Camper Ingrese su ID: ')
+            
+
+            break
+
+    elif opcion == "2":
+        while True:
+            print()
+            print(' ⌨️ Ingreso Trainer ⌨️ ')
+            ID_trainer= input(' Trainer Ingrese su ID: ')
+
+            break
+
+    elif opcion == "3":
+        while True:
+            print()
+            print(' 👽 Ingreso Coordinador ')
+            ID_coordinador= input(' Coordinador Ingrese su ID: ')
+
+            break
+
+    else:
+        while True:
+            opcion == print('❌ Porfavor ingrese una opcion valida ❌')
+            print()
+
+            break
